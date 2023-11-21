897b580af6967649625312f3a87170c466219a45
Jadyn <jadedyan@gmail.com>
2023-11-21 16:59:24 -0700

    refractor

--- scripts/index.js
+++ scripts/index.js
@@ -27,14 +27,42 @@ const initialCards = [
 
 console.log(initialCards);
 
+/*  Events */
+
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseBtn = document.querySelector("#modal-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDesciption = document.querySelector(".profile__description");
+const profileEditTitle = document.querySelector("#profile-title-input");
+const profileEditDescription = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+/* Functions */
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+/* Event Handlers */
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileEditTitle.value;
+  profileDesciption.textContent = profileEditDescription.value;
+  closePopup();
+}
+
+/* Event Listeners */
 
 profileEditBtn.addEventListener("click", () => {
+  profileEditTitle.value = profileTitle.textContent;
+  profileEditDescription.value = profileDesciption.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-profileCloseBtn.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
-});
+profileCloseBtn.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
